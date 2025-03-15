54f60504c2df61848c7bcf05a81fbbaadd9e90f7
PoistJ <157406020+PoistJ@users.noreply.github.com>
2025-03-15 15:49:20 -0400

    Tested functionality of getComputerChoice and debugged conditional

--- script.js
+++ script.js
@@ -1,13 +1,18 @@
 function getComputerChoice() {
     randomNumber = Math.floor(Math.random() * 3);
     
-    if (randomNumber = 0) {
+    if (randomNumber === 0) {
         choice = "rock";
-    } else if (randomNumber = 1) {
+    } else if (randomNumber === 1) {
         choice = "paper";
-    } else if (randomNumber = 2) {
+    } else if (randomNumber === 2) {
         choice = "scissors";
     }
 
     return choice
 }
+
+let computerChoice;
+
+computerChoice = getComputerChoice();
+console.log(computerChoice);
